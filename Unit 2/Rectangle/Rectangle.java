fbf2176837acd9f4db482c31a21a4044e6eb0a1f
AidenPark406 <aidenpark406@gmail.com>
2024-10-17 13:58:23 -0400

    10/17 Added changes to Rectangle (2.6)

--- Unit 2/Rectangle/Rectangle.java
+++ Unit 2/Rectangle/Rectangle.java
@@ -4,9 +4,13 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
-import greenfoot.*;
 
-public class Rectangle extends Actor {
+// breaks in vsc, commented some of these out
+// import greenfoot.*;
+
+// public class Rectangle extends Actor { 
+
+public class Rectangle {
   
   private int width;
   private int height;
@@ -16,13 +20,13 @@ public class Rectangle extends Actor {
   public Rectangle(int w, int h) {
       width = w;
       height = h;
-      getImage().scale(width*10, height*10);
+      // getImage().scale(width*10, height*10);
     }
     
   public Rectangle(int s) {
       width = s;
       height = s;
-      getImage().scale(width*10, height*10);
+      // getImage().scale(width*10, height*10);
 
     }
     
@@ -30,13 +34,26 @@ public class Rectangle extends Actor {
       System.out.println("Width: " + width + " Height: " + height + " partner: " + partner);
     }
     
-  public static void main(String[] args) {
-    Rectangle rect1;
-    rect1 = new Rectangle(12,10);
-    System.out.println(rect1);
-  }
-  
   public String toString() {
     return "Rectangle " + System.identityHashCode(this) + " width "  + width + " height " + height;
+    }
+
+  public int getArea() {
+    int area = width * height;
+    return area;
+  }
+
+  public void printArea() {
+    int area = width * height;
+    System.out.println("The area is " + area);
+  }
+
+
+    public static void main(String[] args) {
+      Rectangle rect1;
+      rect1 = new Rectangle(12,10);
+      System.out.println(rect1);
+      System.out.println(rect1.getArea());
+      rect1.printArea();
     }
 }
